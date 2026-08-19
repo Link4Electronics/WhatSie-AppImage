b8f946acae7b438dacc99d739cc3ede1e7fffcfa
Link4Electronics <link4electronics@gmail.com>
2026-08-19 01:54:35 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -16,8 +16,6 @@ export DEPLOY_PIPEWIRE=1
 # Deploy dependencies
 quick-sharun /usr/bin/whatsie
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
